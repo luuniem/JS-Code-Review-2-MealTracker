79d2bd5fcfdee41f8342a76c8f73b0a77d14586f
Niem Nguyen <luuniem87@gmail.com>
2016-06-02 21:06:37 -0700

    add NewFoodComponent decorator

---
+++ app/new-food.component.ts
@@ -0,0 +1,13 @@
+import { Component, } from 'angular2/core';
+import { Food } from './food.model';
+
+@Component ({
+  selector: 'new-food',
+  template: `
+    <h3> Blank Template</h3>
+  `
+})
+
+export class NewFoodComponent {
+  
+}
