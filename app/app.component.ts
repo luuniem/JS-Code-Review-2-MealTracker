97d157d1e30644f8ae28ec70bfc3f6f5b6dc034f
Niem Nguyen <luuniem87@gmail.com>
2016-06-02 19:05:44 -0700

    add simple decorator and class definition for app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from 'angular2/core';
+import { Food } from './meal.models.ts';
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
+      <h1>Meal Tracker</h1>
     </div>
   `
 })
 
 export class AppComponent {
+  public foods: Food[];
+  constructor(){
+    this.foods = [
+      new Food("Spaghetti", 0),
+      new Food("Big Mac", 1),
+      new Food("Fried Rice", 2)
+    ]
+  }
+  foodWasSelected(clickedFood: Food): void{
+    console.log('parent', clickedFood);
+  }
 }
---
+++ app/meal-list.component.ts
@@ -0,0 +1,2 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Food } from './meal.model';
