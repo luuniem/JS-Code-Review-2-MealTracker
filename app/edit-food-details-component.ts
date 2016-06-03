8cf026bf9b57d4f622a9682678978eb61c437ca4
Niem Nguyen <luuniem87@gmail.com>
2016-06-02 20:38:16 -0700

    add minor styling to input boxes

--- app/edit-food-details-component.ts
+++ app/edit-food-details-component.ts
@@ -5,7 +5,7 @@ import { Food } from './food.model';
   selector: 'edit-food-details',
   inputs: ['food'],
   template: `
-    <div class="keg-form">
+    <div class="food-form">
       <h3> Edit Food Name: </h3>
       <input [(ngModel)]="food.name" class = "col-sm-4 input-lg food-form">
     </div>
