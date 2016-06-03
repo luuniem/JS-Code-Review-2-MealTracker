165bbf2c4bfbbf4fc0fbb418c6fee8aa2bd1fd69
Niem Nguyen <luuniem87@gmail.com>
2016-06-02 20:23:10 -0700

    add FoodListComponent ts

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -1,3 +1,34 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { FoodComponent} from './food.component'
 import { Food } from './food.model';
+import { EditFoodDetailsComponent } from './edit-keg-details.component';
+
+@Component ({
+  selector: 'food-list',
+  inputs: ['foodList'],
+  outputs: ['onFoodSelect'],
+  directives: [FoodComponent, EditFoodDetailsComponent],
+  template: `
+    <food-display *ngFor="#currentFood of foodList"
+    (click)="foodClicked(currentFood)"
+    [class.selected]="currentFood === selectedFood"
+    [food]="currentFood">
+    </food-display>
+    <edit-food-details *ngIf="selectedFood" [food]="selectedFood">
+    </edit-food-details>
+  `
+})
+
+export class FoodListComponent {
+  public foodList: Food[];
+  public onFoodSelect: EventEmitter<Food>;
+  public selectedFood: Food;
+  constructor() {
+    this.onFoodSelect = new EventEmitter();
+  }
+  foodClicked(clickedFood: Food): void {
+    console.log('child', clickedFood);
+    this.selectedFood = clickedFood;
+    this.onFoodSelect.emit(clickedFood)
+  }
+}
